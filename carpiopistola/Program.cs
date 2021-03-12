bb6286dec8c5e4a01c0575ca47aa472b0c08c1ef
AlejandroOrtega17 <00166120@uca.edu.sv>
2021-03-12 16:42:54 -0600

    InitializstartprojectSparta

---
+++ carpiopistola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace carpiopistola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
